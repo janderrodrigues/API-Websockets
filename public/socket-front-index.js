5aedd8570cfa7dccffeb8da07ec7be13b19ab308
janderrodrigues <jander-rodrigues@hotmail.com>
2023-03-23 02:16:11 -0300

    Projeto Websockets

---
+++ public/socket-front-index.js
@@ -0,0 +1,27 @@
+import { inserirLinkDocumento, removerLinkdocumento, } from "./index.js";
+
+const socket = io();
+
+socket.emit("obter_documentos", (documentos) => {
+  documentos.forEach((documento) => {
+    inserirLinkDocumento(documento.nome);
+  });
+});
+
+function emitirAdicionarDocumento(nome) {
+  socket.emit("adicionar_documento", nome);
+}
+
+socket.on("adicionar_documento_interface", (nome) => {
+    inserirLinkDocumento(nome);
+})
+
+socket.on("documento_existente", (nome) => {
+    alert(`O documento ${nome} já existe!`);
+});
+
+socket.on("excluir_documento_sucesso", (nome) => {
+    removerLinkdocumento(nome);
+})
+
+export { emitirAdicionarDocumento };
